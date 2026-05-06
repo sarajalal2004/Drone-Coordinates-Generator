f23471dbb4b196c922bcdce27fbfa4e97646299a
sarajalal2004 <sarauobeducation@gmail.com>
2026-05-06 20:31:37 +0300

    spring boot project

---
+++ src/main/java/com/ga/DroneCoordinatesGenerator/DroneCoordinatesGeneratorApplication.java
@@ -0,0 +1,13 @@
+package com.ga.DroneCoordinatesGenerator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DroneCoordinatesGeneratorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DroneCoordinatesGeneratorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ga/DroneCoordinatesGenerator/DroneCoordinatesGeneratorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ga.DroneCoordinatesGenerator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DroneCoordinatesGeneratorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
