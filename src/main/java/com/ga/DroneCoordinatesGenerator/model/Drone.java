1af81bac61197e6c55f43dc0bbb5d1f674e7163e
sarajalal2004 <sarauobeducation@gmail.com>
2026-05-19 22:05:54 +0300

    Create Drone model

--- src/main/java/com/ga/DroneCoordinatesGenerator/model/Drone.java
+++ src/main/java/com/ga/DroneCoordinatesGenerator/model/Drone.java
@@ -1,4 +1,22 @@
 package com.ga.DroneCoordinatesGenerator.model;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter@Setter
+@Entity
+@Table(name = "drones")
 public class Drone {
+    @Column
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column
+    private String serial;
 }
