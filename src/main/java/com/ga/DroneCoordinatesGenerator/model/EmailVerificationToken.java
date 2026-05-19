6939b62dc1ecbd6004f1350d45ef92774437326b
sarajalal2004 <sarauobeducation@gmail.com>
2026-05-20 01:16:53 +0300

    varification and reset models

---
+++ src/main/java/com/ga/DroneCoordinatesGenerator/model/EmailVerificationToken.java
@@ -0,0 +1,30 @@
+package com.ga.DroneCoordinatesGenerator.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Entity
+@Table(name = "email_verification_token")
+public class EmailVerificationToken {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, nullable = false)
+    private String token;
+
+    @OneToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    private LocalDateTime expiryDate;
+}
